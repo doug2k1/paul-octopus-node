8e8bd519a00421fe577bd12640d079d7ed6b1b53
Douglas Matoso <doug2k1@gmail.com>
2018-05-30 22:21:57 -0300

    Simplify predict algorithm

--- src/models/Match.js
+++ src/models/Match.js
@@ -10,48 +10,35 @@ class Match {
       m =>
         this.home.friends.includes(m.home) && this.away.friends.includes(m.away)
     );
-    const homeFriendsMatchesAsAway = historicalMatches.filter(
-      m =>
-        this.home.friends.includes(m.away) && this.away.friends.includes(m.home)
-    );
     const awayFriendsMatchesAsHome = historicalMatches.filter(
       m =>
         this.away.friends.includes(m.home) && this.home.friends.includes(m.away)
     );
-    const awayFriendsMatchesAsAway = historicalMatches.filter(
-      m =>
-        this.away.friends.includes(m.away) && this.home.friends.includes(m.home)
-    );
 
     let homeScore;
+    let awayScore;
 
     // consider at least 2 historical matches between the two groups
-    if (homeFriendsMatchesAsHome.length + homeFriendsMatchesAsAway.length > 1) {
+    if (homeFriendsMatchesAsHome.length + awayFriendsMatchesAsHome.length > 1) {
       const homeScores = [
         ...homeFriendsMatchesAsHome.map(m => m.home_score),
-        ...homeFriendsMatchesAsAway.map(m => m.away_score)
+        ...awayFriendsMatchesAsHome.map(m => m.away_score)
       ];
       homeScore = Math.round(
         homeScores.reduce((prev, current) => (prev += current), 0) /
           homeScores.length
       );
-    }
 
-    let awayScore;
-
-    // consider at least 2 historical matches between the two groups
-    if (awayFriendsMatchesAsHome.length + awayFriendsMatchesAsAway.length > 1) {
       const awayScores = [
-        ...awayFriendsMatchesAsHome.map(m => m.home_score),
-        ...awayFriendsMatchesAsAway.map(m => m.away_score)
+        ...homeFriendsMatchesAsHome.map(m => m.away_score),
+        ...awayFriendsMatchesAsHome.map(m => m.home_score)
       ];
       awayScore = Math.round(
         awayScores.reduce((prev, current) => (prev += current), 0) /
           awayScores.length
       );
-    }
-
-    if (homeScore === undefined || awayScore === undefined) {
+    } else {
+      // no historical matches, use rank points to predict
       const homePoints = fifaRank.getTeam(this.home.name).points;
       const awayPoints = fifaRank.getTeam(this.away.name).points;
       homeScore = Math.round(Math.min(homePoints / awayPoints, 4));
